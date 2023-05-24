2fc5846823cc5ea3b6ae0facc66297fed88a89c6
Kamen Kanchev <45595147+skyllex94@users.noreply.github.com>
2023-05-24 11:08:19 -0400

    modals completion

--- src/App.js
+++ src/App.js
@@ -12,16 +12,15 @@ function App() {
     <div>
       <div>
         <div className="bg-white pt-20 pb-20 lg:pt-20">
-          <div className="m-auto px-6 xl:container md:px-12 lg:px-6">
-            <h1 className="text-center font-black text-4xl text-black-900 sm:mx-auto sm:w-10/12 sm:text-5xl md:w-2/3 md:text-6xl lg:w-auto lg:text-left xl:text-7xl">
-              NorthVPN
-              <br className="hidden lg:block" />
-              <scan className="text-center text-5xl relative bg-gradient-to-b from-teal-700 to-teal-800 bg-clip-text text-transparent">
-                Your Secure VPN + AdBlocker App
-              </scan>
-            </h1>
-
-            <div className="flex lg:flex">
+          <div className="m-auto flex px-6 xl:container md:px-12 lg:px-6">
+            <div>
+              <h1 className="text-center font-black text-4xl text-black-900 sm:mx-auto sm:w-10/12 sm:text-5xl md:w-2/3 md:text-6xl lg:w-auto lg:text-left xl:text-7xl">
+                NorthVPN
+                <br className="hidden lg:block" />
+                <scan className="text-center text-5xl relative bg-gradient-to-b from-teal-700 to-teal-800 bg-clip-text text-transparent">
+                  Your Secure VPN + AdBlocker App
+                </scan>
+              </h1>
               <div className="relative mt-8 space-y-8 text-center sm:mx-auto sm:w-10/12 md:mt-16 md:w-2/3 lg:ml-0 lg:mr-auto lg:w-7/12 lg:text-left">
                 <p className="text-gray-700 dark:text-gray-300 sm:text-lg lg:w-9/12">
                   Our Virtual Private Network (VPN) is intended to give you one
@@ -59,6 +58,9 @@ function App() {
                   </div>
                 </div>
               </div>
+            </div>
+
+            <div className="flex lg:flex">
               <div className="-right-10 mt-12 md:mt-0 lg:w-7/12">
                 <div className="flex justify-center">
                   <img
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import ContactsModal from "./Modals/ContactsModal";
+import AboutModal from "./Modals/AboutModal";
+import PolicyModal from "./Modals/PolicyModal";
 
 export default function Footer() {
+  const [showModal, setShowModal] = useState(false);
+  const [showAboutModal, setShowAboutModal] = useState(false);
+  const [showPolicyModal, setShowPolicyModal] = useState(false);
+
   return (
-    <div class=" bg-gray-900 text-white">
-      <div class="max-w-2xl mx-auto py-10">
-        <div class="text-center">
-          <h3 class="text-3xl mb-3 text-white">
-            Download our VPN + Adblocker App
-          </h3>
-          <p> Stay secure. All day, every day.</p>
-          <div class="flex justify-center my-10">
-            <div class="flex items-center border w-auto rounded-lg px-4 py-2  mx-2">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
-                class="w-7 md:w-8"
-                alt="ios"
-              />
-              <a
-                href="https://apps.apple.com/us/app/vpn-secure-proxy-shield/id1459783875"
-                target="_blank"
-                rel="noreferrer"
-                class="text-left ml-3"
+    <React.Fragment>
+      <div className=" bg-gray-900 text-white">
+        <div className="max-w-2xl mx-auto py-10">
+          <div className="text-center">
+            <h3 className="text-3xl mb-3 text-white">
+              Download our VPN + Adblocker App
+            </h3>
+            <p> Stay secure. All day, every day.</p>
+            <div className="flex justify-center my-10">
+              <div className="flex items-center border w-auto rounded-lg px-4 py-2  mx-2">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
+                  className="w-7 md:w-8"
+                  alt="ios"
+                />
+                <a
+                  href="https://apps.apple.com/us/app/vpn-secure-proxy-shield/id1459783875"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-left ml-3"
+                >
+                  <p className="text-xs text-gray-200">Download on </p>
+                  <p className="text-sm md:text-base"> Apple Store </p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+            <p className="order-2 md:order-1 mt-8 md:mt-0">
+              &copy; Zion Studios LLC., {new Date().getFullYear()}.
+            </p>
+            <div className="order-1 md:order-2">
+              <button onClick={() => setShowModal(true)} className="px-2">
+                About us
+              </button>
+              <button
+                onClick={() => setShowAboutModal(true)}
+                className="px-2 border-l"
               >
-                <p class="text-xs text-gray-200">Download on </p>
-                <p class="text-sm md:text-base"> Apple Store </p>
-              </a>
+                Contact us
+              </button>
+              <button
+                onClick={() => setShowPolicyModal(true)}
+                className="px-2 border-l"
+              >
+                Privacy Policy
+              </button>
             </div>
           </div>
         </div>
-        <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-          <p class="order-2 md:order-1 mt-8 md:mt-0">
-            &copy; Zion Studios LLC., {new Date().getFullYear()}.
-          </p>
-          <div class="order-1 md:order-2">
-            <span class="px-2">About us</span>
-            <span class="px-2 border-l">Contact us</span>
-            <span class="px-2 border-l">Privacy Policy</span>
-          </div>
-        </div>
       </div>
-    </div>
+      {showModal && <AboutModal setShowModal={setShowModal} />}
+      {showAboutModal && <ContactsModal setShowModal={setShowAboutModal} />}
+      {showPolicyModal && <PolicyModal setShowModal={setShowPolicyModal} />}
+    </React.Fragment>
   );
 }
